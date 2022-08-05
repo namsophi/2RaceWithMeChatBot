9b67510091a10ce9955cb5fa3d34db3a2156ba85
snam <s.nam@mail.utoronto.ca>
2022-08-05 10:52:37 -0400

    Edit weather functionality

--- scripts/weather.py
+++ scripts/weather.py
@@ -1,20 +1,23 @@
 from pyowm.owm import OWM
 from scripts.utils import text_to_speech
+from geopy.geocoders import Nominatim
 import datetime
 import geocoder
 
 # TODO: Fix api key exposure
 owm = OWM('e9dd1343ae35d3b8db26656b78b75c45')
 mgr = owm.weather_manager()
+geolocator = Nominatim(user_agent="MyApp")
 
 
 def get_current_weather(city):
-    g = geocoder.google(city)
-    one_call = mgr.one_call(lat=g.latlng[0], lon=g.latlng[1])
+    g = geolocator.geocode(city)
+    one_call = mgr.one_call(lat=g.latitude, lon=g.longitude)
     weather_data = one_call.current
     response = _get_weather_statements(one_call.timezone.split("/")[1],
                                        weather_data)
-    text_to_speech(response)
+    print(response)
+    text_to_speech(' '.join(response))
     return response
 
 
