9732035bdf5241cdcce39c70fc697b4a7d3f23ae
S <s.nam@mail.utoronto.ca>
2022-08-22 10:58:17 -0400

    Fix spacing

--- scripts/location.py
+++ scripts/location.py
@@ -11,19 +11,19 @@ def get_location_data(city, country):
         response = f"Welcome to {city}, a city in {country}! " \
                    f"{country} is a country that is {country_size} " \
                    f"square kilometers big. {capital} is the capital city of " \
-                   f"{country}, and there are no" \
+                   f"{country}, and there are no " \
                    f"countries bordering {country}. In this video we will explore"
     elif bordering_countries == 1:
         response = f"Welcome to {city}, a city in {country}! " \
                    f"{country} is a country that is {country_size} " \
                    f"square kilometers big. {capital} is the capital city of " \
-                   f"{country}, and there is one" \
+                   f"{country}, and there is one " \
                    f"country bordering {country}. In this video we will explore"
     else:
         response = f"Welcome to {city}, a city in {country}! " \
                    f"{country} is a country that is {country_size} " \
                    f"square kilometers big. {capital} is the capital city of " \
-                   f"{country}, and there are {bordering_countries}" \
+                   f"{country}, and there are {bordering_countries} " \
                    f"countries bordering {country}. In this video we will explore"
     text_to_speech(response)
     return response
